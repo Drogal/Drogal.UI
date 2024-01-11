a5bda90b18315ce897a8de17f50b29bd11315a12
niltonDrogal <nilton.rodrigues@drogal.com.br>
2024-01-11 20:19:46 -0300

    ...

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// export * from './Components/Button/Button';
+// export * from './Components/Card/Card';
+// export * from './Components/Input/Input';
+// export * from './Components/Input/TextArea';
+// export * from './Components/Modal/Modal';
+// export * from './Components/RadioButton/RadioButton';
+
+
+import Button from  './Components/Button/Button'
+import Card from  './Components/Card/Card'
+import Input from  './Components/Input/Input'
+import TextArea from  './Components/Input/TextArea'
+import Modal from  './Components/Modal/Modal'
+import RadioButton from  './Components/RadioButton/RadioButton'
+
+export {
+  Button,
+  Card,
+  Input,
+  TextArea,
+  Modal,
+  RadioButton
+}
